9096a3cd3687bb4a93b75842ec050dcd39ca5c5b
Maria Avila <126889097+MariaAvilaConde@users.noreply.github.com>
2025-05-22 14:37:36 +0000

    limpiar

--- src/main/java/edu/pe/vallegrande/TranslateAI/repository/CopichatRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.pe.vallegrande.TranslateAI.repository;
-
-import edu.pe.vallegrande.TranslateAI.model.Copichat;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Flux;
-
-public interface CopichatRepository extends ReactiveCrudRepository<Copichat, Long> {
-    Flux<Copichat> findByStatus(char status);
-}
